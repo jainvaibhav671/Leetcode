19ced5266020a25ff9cf918e539d92aa8be60fd9
cse-vaibhav <jainvaibhav671@gmail.com>
2023-03-05 23:37:39 +0530

    Solved some problems

---
+++ all-problems/single-number-ii.py
@@ -0,0 +1,9 @@
+from typing import List
+
+
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        for x in nums:
+            if nums.count(x) == 1:
+                return x
+        return -1
