799773fe5634c33cc81e06eca46a6d3f8684ee5c
cse-vaibhav <jainvaibhav671@gmail.com>
2023-03-13 10:14:25 +0530

    Completed March23 day13 problem

---
+++ March2023/day13.py
@@ -0,0 +1,34 @@
+
+from typing import Optional
+
+# Definition for a binary tree node.
+
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution:
+
+    def traverse(self, left: Optional[TreeNode], right: Optional[TreeNode]):
+        if not left and not right:
+            return True
+        elif not left or not right:
+            return False
+
+        if left.val != right.val:
+            return False
+
+        _left = self.traverse(left.right, right.left)
+        _right = self.traverse(left.left, right.right)
+
+        if _left and _right:
+            return True
+
+        return False
+
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        return self.traverse(root.left, root.right)
