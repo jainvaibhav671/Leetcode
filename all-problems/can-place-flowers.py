bcc3b3dc930a247eab4400ed5490a947ff0cda20
cse-vaibhav <jainvaibhav671@gmail.com>
2023-03-20 12:16:34 +0530

    Completed March2023 day20 problem

---
+++ all-problems/can-place-flowers.py
@@ -0,0 +1,18 @@
+
+from typing import List
+
+
+class Solution:
+    def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
+        count = 0
+
+        for i in range(len(flowerbed)):
+            if flowerbed[i] == 0:
+                left_plot = (i == 0) or (flowerbed[i-1] == 0)
+                right_plot = (i == len(flowerbed) - 1) or (flowerbed[i+1] == 0)
+
+                if left_plot and right_plot:
+                    flowerbed[i] = 1
+                    count += 1
+
+        return count >= n
